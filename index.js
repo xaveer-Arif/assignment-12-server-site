f5a40d5de460378a0091681477dec7ff1bf10e67
xaveerarif <xaveerarif@gmail.com>
2021-11-12 20:05:51 +0600

    post method

--- index.js
+++ index.js
@@ -20,12 +20,19 @@ async function run(){
         await client.connect();
         const database = client.db('Appartment-istria');
         const allCollection = database.collection('allAppartments')
-
+        const myOrder = database.collection('myOrder')
         // find all
         app.get('/services', async(req, res)=> {
             const cursor = await allCollection.find({}).toArray();
             console.log(cursor)
             res.json(cursor) 
+        })
+        // post purchase order in myOrder
+        app.post('/order', async(req, res) => {
+            const data = req.body
+            const result = await myOrder.insertOne(data)
+            console.log('aita my order command', result)
+            res.json(result)
         })
 
     }   
@@ -37,6 +44,7 @@ async function run(){
 run().catch(console.dir);
 
 app.get('/', (req, res)=> {
+    console.log('command')
     res.send('hellow world')
 })
 app.listen(port, () => {
